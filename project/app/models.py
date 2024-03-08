0c34c688fcd1949694e2b663f2e32268185ccf8b
Ale <alexandra_filipas@yahoo.com>
2024-03-08 21:37:12 +0200

    Initial model setup

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Aliment(models.Model):
+    titlu = models.CharField(max_length=50, unique=True)
+    stoc = models.IntegerField(default=0, db_index=True)
+    calorii = models.IntegerField(default=0, db_index=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Aliment {self.titlu} "
+    
+class Retete(models.Model):
+    aliment = models.ForeignKey(Aliment, null=True, blank=True, on_delete=models.CASCADE)
+    nume = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Retete {self.nume}"
